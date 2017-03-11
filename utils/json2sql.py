e8a18fe5fbb30758dd29fd25d168a6f14f9807ed
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-11 12:59:41 +0800

    Modulize the script, split into json2sql and main functions.

--- utils/json2sql.py
+++ utils/json2sql.py
@@ -4,68 +4,79 @@ import json
 import os
 import optparse
 
-# set up option parser
-parser = optparse.OptionParser()
-parser.set_default('table_name', 'TABLE')
-parser.set_default('json_path', './data.json')
-parser.set_default('sql_path', '')
-parser.add_option('-t', '--table',
-                  action='store', type='string', dest='table_name',
-                  help='The name of the table to be created or inserted.')
-parser.add_option('-j', '--json',
-                  action='store', type='string', dest='json_path',
-                  help='The path of the json file.')
-parser.add_option('-s', '--sql',
-                  action='store', type='string', dest='sql_path',
-                  help='The path of the output sql file.')
 
-(options, args) = parser.parse_args()
+def json2sql(json_path, sql_path, table_name, encoding='utf-8', is_multi_line=False):
 
-table_name = options.table_name
-
-json_path = options.json_path
-
-sql_path = ''
-
-if options.sql_path == '':
-    (root, ext) = os.path.splitext(json_path)
-    sql_path = root + '.sql'
-
-# read the data
-data = []
-with codecs.open(json_path, 'r', encoding='utf-8') as f:
-    for line in f:
-        data.append(json.loads(line))
-
-# get the keys to creat the table headers
-keys = list(data[0].keys())
-
-# open the output file
-out_file = codecs.open(sql_path, 'w', encoding='utf-8')
-
-# write the create table
-out_file.write('CREATE TABLE IF NOT EXISTS `%s` (\n' % table_name)
-out_file.write('\t`TABLE_ID` int(11) NOT NULL auto_increment,\n')
-for key in keys:
-    out_file.write('\t`%s` varchar(128) default NULL,\n' % key)
-out_file.write('\tPRIMARY KEY (`TABLE_ID`)\n')
-out_file.write(')DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;\n\n')
-
-# write insert statement
-for item in data:
-    out_file.write('INSERT INTO %s(' % table_name)
-    for i in range(len(keys)):
-        if i == len(keys) - 1:
-            out_file.write('%s' % keys[i])
+    # read the data
+    data = []
+    with codecs.open(json_path, 'r', encoding=encoding) as f:
+        if is_multi_line:
+            for line in f:
+                data.append(json.loads(line))
         else:
-            out_file.write('%s, ' % keys[i])
+            data = json.loads(f)
 
-    out_file.write(') VALUES (')
+    # get the keys to creat the table headers
+    keys = list(data[0].keys())
 
-    for i in range(len(keys)):
-        if i == len(keys) - 1:
-            out_file.write('\"%s\"' % item[keys[i]])
-        else:
-            out_file.write('\"%s\", ' % item[keys[i]])
+    # open the output file
+    out_file = codecs.open(sql_path, 'w', encoding=encoding)
 
-    out_file.write(');\n')
+    # write the create table
+    out_file.write('CREATE TABLE IF NOT EXISTS `%s` (\n' % table_name)
+    out_file.write('\t`TABLE_ID` int(11) NOT NULL auto_increment,\n')
+    for key in keys:
+        out_file.write('\t`%s` varchar(128) default NULL,\n' % key)
+    out_file.write('\tPRIMARY KEY (`TABLE_ID`)\n')
+    out_file.write(')DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;\n\n')
+
+    # write insert statement
+    for item in data:
+        out_file.write('INSERT INTO %s(' % table_name)
+        for i in range(len(keys)):
+            if i == len(keys) - 1:
+                out_file.write('%s' % keys[i])
+            else:
+                out_file.write('%s, ' % keys[i])
+
+        out_file.write(') VALUES (')
+
+        for i in range(len(keys)):
+            if i == len(keys) - 1:
+                out_file.write('\"%s\"' % item[keys[i]])
+            else:
+                out_file.write('\"%s\", ' % item[keys[i]])
+
+        out_file.write(');\n')
+
+
+def main():
+    # set up option parser
+    parser = optparse.OptionParser()
+    parser.add_option('-t', '--table',
+                      action='store', dest='table_name', default='TABLE',
+                      help='The name of the table to be created or inserted.')
+    parser.add_option('-j', '--json',
+                      action='store', dest='json_path', default= './data.json',
+                      help='The path of the json file.')
+    parser.add_option('-s', '--sql',
+                      action='store', dest='sql_path',
+                      help='The path of the output sql file.')
+    parser.add_option('-l', '--lines',
+                      action='store_true', dest='is_multi_line', default=False,
+                      help='Whether the file contains many objects line by line or a big json object.')
+    parser.add_option('-e', '--encoding',
+                      action='store', dest='encoding', default='utf-8',
+                      help='The encoding of the json and sql file.')
+
+    (options, args) = parser.parse_args()
+
+    if not options.sql_path:
+        (root, ext) = os.path.splitext(options.json_path)
+        options.sql_path = root + '.sql'
+
+    json2sql(options.json_path, options.sql_path, options.table_name,
+             encoding=options.encoding, is_multi_line=options.is_multi_line)
+
+if __name__ == '__main__':
+    main()
