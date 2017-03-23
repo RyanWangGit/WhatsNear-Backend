03312165a4e1b6de02a8e12c01188b9c5d589902
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-23 14:19:30 +0800

    Implement csvreader.

---
+++ whatsnear_backend/readers.py
@@ -0,0 +1,9 @@
+def csvreader(file_path, encoding='utf-8'):
+    import csv
+    import codecs
+
+    with codecs.open(file_path, 'r', encoding=encoding) as csv_file:
+        csv_dict = csv.DictReader(csv_file, restkey=None, restval=None)
+        out = [obj for obj in csv_dict]
+
+    return out
