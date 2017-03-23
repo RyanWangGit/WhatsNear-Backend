1401093b39031d960c10751c83c95800b90d4bdf
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-23 15:18:23 +0800

    Remove `codecs`.

--- whatsnear_backend/readers.py
+++ whatsnear_backend/readers.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-def csvreader(file_path, encoding='utf-8'):
+def csvread(file_path):
     import csv
-    import codecs
 
-    with codecs.open(file_path, 'r', encoding=encoding) as csv_file:
-        csv_dict = csv.DictReader(csv_file, restkey=None, restval=None)
-        out = [obj for obj in csv_dict]
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file, restkey=None, restval=None)
+        out = [row for row in reader]
 
     return out
