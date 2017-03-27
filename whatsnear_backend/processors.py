089dd8ed5e364cd1bbe4c415eece52a4a5642c26
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-27 12:50:02 +0800

    Commit some classic preprocessor and an empty_processor.

--- whatsnear_backend/processors.py
+++ whatsnear_backend/processors.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+def filter_preprocessor(points, can_pass):
+    return [point for point in points if can_pass(point)]
+
+
+def sample_preprocessor(points, sample_num):
+    import random
+    return random.sample(points, sample_num)
+
+
+def empty_processor(points):
+    nodes = []
+    edges = []
+    infos = []
+    return nodes, infos, edges
