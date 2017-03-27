fea2c81d9582f7dd97b5f0fb192266717ca79bf6
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-27 22:42:57 +0800

    Implement csvwriter.

--- whatsnear_backend/writers.py
+++ whatsnear_backend/writers.py
@@ -6,3 +6,22 @@ def console_writer(nodes, infos, edges):
     print(edges)
     print('infos:')
     print(infos)
+
+
+def csvwriter(nodes, infos, edges, path):
+    if not path.endswith('/'):
+        path += '/'
+
+    with open(path + 'nodes.csv', 'w') as file:
+        for node in nodes:
+            file.write('%d,%s,%s,%d\n' % (node['index'], node['lng'], node['lat'], node['value']))
+
+    with open(path + 'infos.csv', 'w') as file:
+        for info in infos:
+            file.write('%d,%s,%s,%s\n' % (info['index'], info['name'], info['address'], info['telephone']))
+
+    with open(path + 'edges.csv', 'w') as file:
+        for edge in edges:
+            file.write('%d,%d\n' % (edge[0], edge[1]))
+
+
