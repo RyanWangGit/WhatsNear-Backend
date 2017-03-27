9f542f58fee65edb3a542351449465592e9719b2
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-27 12:51:28 +0800

    Add a console writer.

--- whatsnear_backend/writers.py
+++ whatsnear_backend/writers.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+def console_writer(nodes, infos, edges):
+    print('nodes:')
+    print(nodes)
+    print('edges:')
+    print(edges)
+    print('infos:')
+    print(infos)
