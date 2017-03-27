ce487c401b3ad75c958ec6f0106a0464b4ba9339
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-03-27 22:42:42 +0800

    Implement random processor.

--- whatsnear_backend/processors.py
+++ whatsnear_backend/processors.py
@@ -8,8 +8,32 @@ def sample_preprocessor(points, sample_num):
     return random.sample(points, sample_num)
 
 
-def empty_processor(points):
+def random_processor(points, edge_num):
+    import random
     nodes = []
     edges = []
     infos = []
+    for index in range(len(points)):
+        point = points[index]
+        nodes.append({
+            'index': index,
+            'lng': point['lng'],
+            'lat': point['lat'],
+            'value': 0
+        })
+        infos.append({
+            'index': index,
+            'name': point['name'],
+            'address': point['address'],
+            'telephone': point['telephone']
+        })
+
+    for _ in range(edge_num):
+        sample = random.sample(nodes, 2)
+
+        sample[0]['value'] += 1
+        sample[1]['value'] += 1
+
+        edges.append((sample[0]['index'], sample[1]['index']))
+
     return nodes, infos, edges
