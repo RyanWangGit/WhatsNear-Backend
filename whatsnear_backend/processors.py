f05559fea370902cf20ab95a1e9e5892f7bc6755
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-04-05 14:04:36 +0800

    Generate edges before split the point properties and trim those points without any connections.

--- whatsnear_backend/processors.py
+++ whatsnear_backend/processors.py
@@ -14,35 +14,45 @@ def random_processor(points, edge_num):
     edges = []
     infos = []
 
-    for index in range(len(points)):
-        point = points[index]
-        nodes.append({
-            'index': index,
-            'lng': point['lng'],
-            'lat': point['lat'],
-            'value': 0
-        })
-        infos.append({
-            'index': index,
-            'name': point['name'],
-            'address': point['address'],
-            'telephone': point['telephone']
-        })
+    small_sample = random.sample(points, int(len(points) * 0.1))
 
-    small_sample = random.sample(nodes, int(len(nodes) * 0.1))
-
-    edges_map = {}
+    # generate edges
+    edges_list = []
     for i in range(edge_num):
         if i < edge_num * 0.01:
-            sample = random.sample(nodes, 2)
+            sample = random.sample(points, 2)
         else:
             sample = random.sample(small_sample, 2)
 
         sample[0]['value'] += 1
         sample[1]['value'] += 1
 
-        key = '%d,%d' % (min(sample[0]['index'], sample[1]['index']),
-                         max(sample[0]['index'], sample[1]['index']))
+        edges_list.append(sample)
+
+    # filter out those have no connections with other points
+    # and mark the point
+    index = 0
+    for point in points:
+        if point['value'] > 0:
+            nodes.append({
+                'index': index,
+                'lng': point['lng'],
+                'lat': point['lat'],
+                'value': point['value']
+            })
+            infos.append({
+                'index': index,
+                'name': point['name'],
+                'address': point['address'],
+                'telephone': point['telephone']
+            })
+            point['index'] = index
+            index += 1
+
+    edges_map = {}
+    for edge in edges_list:
+        key = '%d,%d' % (min(edge[0]['index'], edge[1]['index']),
+                         max(edge[0]['index'], edge[1]['index']))
 
         if key in edges_map:
             edges_map[key] += 1
