9d1b821cb6e73e5ec6d05ad66821d277e31c4413
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-05-08 16:23:34 +0800

    Implement dumb HTTP server.

--- whatsnear/__main__.py
+++ whatsnear/__main__.py
@@ -1,14 +1,13 @@
-from whatsnear import WhatsNear
+import whatsnear
 
 if __name__ == '__main__':
-    backend = WhatsNear()
-    import argparse
-
     # set up argument parser
+    import argparse
     parser = argparse.ArgumentParser(description='Backend for WhatsNear.')
     parser.add_argument('-p', '--port',
                         action='store', dest='port', default=80, type=int,
                         help='The port to listen on.', required=False)
-
     results = parser.parse_args()
-    backend.start(results.port)
+
+    # start server
+    whatsnear.start_server(results.port)
--- whatsnear/whatsnear.py
+++ whatsnear/whatsnear.py
@@ -1,6 +1,32 @@
-class WhatsNear:
-    def __init__(self):
-        pass
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
-    def start(self, port = 8080):
-        print 'hello'
+
+class WhatsNearHTTPHandler(BaseHTTPRequestHandler):
+    def parse_parameters(self):
+        path = ''
+        params = {}
+        if '?' in self.path:
+            path, args = self.path.split('?')
+            for param in args.split('&'):
+                name, value = param.split('=')
+                params[name] = value
+
+        return path, params
+
+    def do_GET(self):
+        path, params = self.parse_parameters()
+
+        # prepare the header
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        # prepare the content
+        if path == '/query':
+            self.wfile.write('Hello')
+
+
+def start_server(port=8080):
+    http_server = HTTPServer(('127.0.0.1', int(port)), WhatsNearHTTPHandler)
+    http_server.serve_forever()
