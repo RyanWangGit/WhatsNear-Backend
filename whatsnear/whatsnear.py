288a5d5b0519f604267e012446ca492259092d7d
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-05-09 23:07:39 +0800

    Use tornado framework to do the work.

--- whatsnear/whatsnear.py
+++ whatsnear/whatsnear.py
@@ -1,56 +1,33 @@
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import tornado.ioloop
+import tornado.web
 import json
 from ranknet import RankNet
 
-
-class WhatsNearHTTPHandler(BaseHTTPRequestHandler):
-    def parse_parameters(self):
-        path = self.path
-        params = {}
-        if '?' in self.path:
-            path, args = self.path.split('?')
-            if not args == '':
-                for param in args.split('&'):
-                    name, value = param.split('=')
-                    params[name] = value
-
-        return path, params
-
-    def do_GET(self):
-        path, params = self.parse_parameters()
-
-        # prepare the header
-        self.protocol_version = 'HTTP/1.1'
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-        # prepare the content
-        if path == '/' or path == '':
-            self.wfile.write(self.respond_help())
-        elif path == '/query':
-            self.wfile.write(self.respond_query(params))
-
-    def respond_query(self, params):
-        if 'points' not in params:
-            return 'Not enough parameters.'
-
-        query_points = json.loads(params['points'])
-
-        return json.dumps(ranknet.rank(query_points))
-
-    def respond_help(self):
-        return \
-            'Usage: \n' \
-            '/query - [[lng, lat], [lng, lat] ...]'
-
+# global ranknet object
 ranknet = RankNet()
+
+class WhatsNearHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('Usage: \n' +
+                   '/query - [[lng, lat], [lng, lat] ...]')
+
+
+class QueryHandler(tornado.web.RequestHandler):
+    def get(self):
+        query_points = self.get_argument('points')
+        self.add_header('Content-type', 'application/json')
+        self.write(json.dumps(ranknet.rank(query_points)))
 
 
 def start_server(points, port=8080):
     ranknet.load(points)
 
+    app = tornado.web.Application([
+        ('/', WhatsNearHandler),
+        ('/query', QueryHandler)
+    ])
+
     ip = '127.0.0.1'
-    http_server = HTTPServer((ip, int(port)), WhatsNearHTTPHandler)
+    app.listen(port, ip)
     print('Start hosting at http://%s:%d.' % (ip, port))
-    http_server.serve_forever()
+    tornado.ioloop.IOLoop.current().start()
