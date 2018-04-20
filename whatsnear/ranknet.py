06f6cc68c4616df6c293396dc8c09d821c04f44c
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-04-20 18:27:32 -0400

    Use embedded keras of tensorflow.

--- whatsnear/ranknet.py
+++ whatsnear/ranknet.py
@@ -1,8 +1,8 @@
-import tensorflow as tf
 # -*- coding: utf-8 -*-
 import random
 import time
 from six.moves import xrange
+import numpy as np
 
 
 class RankNet(object):
@@ -46,93 +46,14 @@ class RankNet(object):
 
         return (np.array(X1), np.array(X2)), (np.array(Y1), np.array(Y2))
 
-    def _tf_train_model(self):
-
-        feature_num = 10
-        h1_num = 10
-
-        with tf.name_scope("input"):
-            x1 = tf.placeholder(tf.float32, [None, feature_num], name="x1")
-            x2 = tf.placeholder(tf.float32, [None, feature_num], name="x2")
-
-            o1 = tf.placeholder(tf.float32, [None, 1], name="o1")
-            o2 = tf.placeholder(tf.float32, [None, 1], name="o2")
-
-            # add layer1
-        with tf.name_scope("layer1"):
-            with tf.name_scope("w1"):
-                w1 = tf.Variable(tf.random_normal([feature_num, h1_num]), name="w1")
-                # tf.summary.histogram("layer1/w1", w1)
-            with tf.name_scope("b1"):
-                b1 = tf.Variable(tf.random_normal([h1_num]), name="b1")
-                # tf.summary.histogram("layer1/b1", b1)
-
-            # didn't add activation function
-            with tf.name_scope("h1_o1"):
-                h1_o1 = tf.matmul(x1, w1) + b1
-                # tf.summary.histogram("h1_o1", h1_o1)
-
-            with tf.name_scope("h2_o1"):
-                h1_o2 = tf.matmul(x2, w1) + b1
-                # tf.summary.histogram("h2_o1", h1_o2)
-
-                # add output layer
-        with tf.name_scope("output"):
-            with tf.name_scope("w2"):
-                w2 = tf.Variable(tf.random_normal([h1_num, 1]), name="w2")
-                # tf.summary.histogram("output/w2", w2)
-
-            with tf.name_scope("b2"):
-                b2 = tf.Variable(tf.random_normal([1]))
-                # tf.summary.histogram("output/b2", b2)
-
-            h2_o1 = tf.matmul(h1_o1, w2) + b2
-            h2_o2 = tf.matmul(h1_o2, w2) + b2
-
-            # calculate probability based on output layer
-        with tf.name_scope("loss"):
-            o12 = o1 - o2
-            h_o12 = h2_o1 - h2_o2
-
-            pred = 1 / (1 + tf.exp(-h_o12))
-            lable_p = 1 / (1 + tf.exp(-o12))
-
-            cross_entropy = -lable_p * tf.log(pred) - (1 - lable_p) * tf.log(1 - pred)
-            reduce_sum = tf.reduce_sum(cross_entropy, 1)
-            loss = tf.reduce_mean(reduce_sum)
-            tf.summary.scalar("loss", loss)
-
-        with tf.name_scope("train_op"):
-            train_op = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-
-        with tf.Session() as sess:
-            # summary_op = tf.summary.merge_all()
-            # writer = tf.summary.FileWriter("./logs/", sess.graph)
-            init = tf.global_variables_initializer()
-            sess.run(init)
-            for epoch in range(0, 100):
-                X, Y = self.get_train_data()
-                sess.run(train_op, feed_dict={x1: X[0], x2: X[1], o1: Y[0], o2: Y[1]})
-                if epoch % 10 == 0:
-                    # summary_result = sess.run(summary_op, feed_dict={x1: X[0], x2: X[1], o1: Y[0], o2: Y[1]})
-                    # writer.add_summary(summary_result, epoch)
-                    l_v = sess.run(loss, feed_dict={x1: X[0], x2: X[1], o1: Y[0], o2: Y[1]})
-                    h_o12_v = sess.run(h_o12, feed_dict={x1: X[0], x2: X[1], o1: Y[0], o2: Y[1]})
-                    o12_v = sess.run(o12, feed_dict={x1: X[0], x2: X[1], o1: Y[0], o2: Y[1]})
-                    # print "------ epoch[%d] loss_v[%f] ------ " % (epoch, l_v)
-                    # for k in range(0, len(o12_v)):
-                    # print "k[%d] o12_v[%f] h_o12_v[%f]" % (k, o12_v[k], h_o12_v[k])
-
-    def _keras_train_model(self, features, labels, epochs=10, batches=10):
+    def _train_model(self, features, labels, epochs=10, batches=10):
         # Michael A. Alcorn (malcorn@redhat.com)
         # A (slightly modified) implementation of RankNet as described in [1].
         #   [1] http://icml.cc/2015/wp-content/uploads/2015/06/icml_ranking.pdf
         #   [2] https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/MSR-TR-2010-82.pdf
-        import numpy as np
-
-        from keras import backend
-        from keras.layers import Activation, Add, Dense, Input, Lambda
-        from keras.models import Model
+        from tensorflow.python.keras import backend
+        from tensorflow.python.keras.layers import Activation, Add, Dense, Input, Lambda
+        from tensorflow.python.keras.models import Model
 
         dimension = len(features[0])
 
@@ -193,7 +114,7 @@ class RankNet(object):
         self._score_function = backend.function([rel_doc], [rel_score])
 
     def load(self, path):
-        from keras.models import load_model
+        from tensorflow.python.keras.models import load_model
         print('[TensorFlow] Trained model file found, loading model...')
         self._model = load_model(path)
         self._is_ready = True
@@ -205,7 +126,6 @@ class RankNet(object):
         print('[TensorFlow] Model saved to %s' % path)
 
     def ndcg(self, y_true, y_score, k=10):
-        import numpy as np
         y_true = y_true.ravel()
         y_score = y_score.ravel()
         y_true_sorted = sorted(y_true, reverse=True)
@@ -226,13 +146,12 @@ class RankNet(object):
         print('[TensorFlow] Start training model...')
         start_time = time.clock()
         self._dataset = dataset
-        import random
         train_features = dataset.get_features()[:int(len(dataset.get_features()) * rate)]
         train_labels = dataset.get_labels()[:int(len(dataset.get_features()) * rate)]
         test_features = dataset.get_features()[int(len(dataset.get_features()) * rate):]
         test_labels = dataset.get_labels()[int(len(dataset.get_features()) * rate):]
 
-        self._keras_train_model(train_features, train_labels, epochs=epochs, batches=batches)
+        self._train_model(train_features, train_labels, epochs=epochs, batches=batches)
         self._is_ready = True
         end_time = time.clock()
         print('[TensorFlow] Model trained in %f seconds' % (end_time - start_time))
@@ -241,7 +160,6 @@ class RankNet(object):
         test_range = range(len(test_features))
 
         ndcg = 0
-        import numpy as np
         for _ in range(1000):
             test = random.sample(test_range, 10)
             to_rank_features = []
