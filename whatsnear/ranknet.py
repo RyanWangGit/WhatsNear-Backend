7cd53c2e3ffbe44b0bb3a98a4d1be528c0dbd021
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-04-24 14:10:23 -0400

    Remove dead code.

--- whatsnear/ranknet.py
+++ whatsnear/ranknet.py
@@ -15,35 +15,6 @@ class RankNet(object):
         # score function
         self._score_function = None
 
-    def get_train_data(self, batch_size=32):
-        import numpy as np
-
-        # generate data with 10 dimensions
-        X1, X2 = [], []
-        Y1, Y2 = [], []
-
-        for i in range(0, batch_size):
-            x1 = []
-            x2 = []
-            o1 = 0.0
-            o2 = 0.0
-            for j in range(0, 10):
-                r1 = random.random()
-                r2 = random.random()
-                x1.append(r1)
-                x2.append(r2)
-
-                mu = 2.0
-                if j >= 5: mu = 3.0
-                o1 += r1 * mu
-                o2 += r2 * mu
-            X1.append(x1)
-            Y1.append([o1])
-            X2.append(x2)
-            Y2.append([o2])
-
-        return (np.array(X1), np.array(X2)), (np.array(Y1), np.array(Y2))
-
     def _train_model(self, features, labels, epochs=10, batches=10):
         # Michael A. Alcorn (malcorn@redhat.com)
         # A (slightly modified) implementation of RankNet as described in [1].
@@ -90,7 +61,6 @@ class RankNet(object):
         x1 = []
         x2 = []
         y = []
-        indexes = range(len(features))
         for i in range(len(features) - 1):
             x1.append(features[i])
             x2.append(features[i + 1])
