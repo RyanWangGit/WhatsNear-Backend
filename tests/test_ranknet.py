0e3c379f7db1ce13cc78ca1d89b9add9410dd633
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-04-19 00:01:39 -0400

    Remove unnecessary import and setup of coloredlogs.

--- tests/test_ranknet.py
+++ tests/test_ranknet.py
@@ -1,9 +1,6 @@
 from ranknear.dataset import Dataset
 from ranknear.ranknet import RankNet
-import coloredlogs
 import numpy as np
-
-coloredlogs.install('DEBUG', fmt='%(asctime)s %(name)s[0x%(process)x] %(levelname)s %(message)s')
 
 
 def test_ranknet():
@@ -12,4 +9,3 @@ def test_ranknet():
     ranknet = RankNet()
     ndcg = ranknet.train(np.array(dataset.get_features()), np.array(dataset.get_labels()), 0.8)
     assert ndcg > 0.3
-
