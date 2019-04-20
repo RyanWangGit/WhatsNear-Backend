34d4e3393eeefbcdc2b45617d3f54b38f4d5bfa6
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-04-19 22:21:59 -0400

    Rename for better readability.

--- ranknear/ranknet.py
+++ ranknear/ranknet.py
@@ -92,10 +92,8 @@ class RankNet:
         y_true_sorted = np.sort(y_true)[::-1]
         ideal_dcg = sum((2 ** y_true_sorted[i] - 1.) / np.log2(i + 2) for i in range(k))
 
-        dcg = 0
-        argsort_indices = np.argsort(y_score)[::-1]
-
-        dcg += sum((2 ** y_true[argsort_indices[i]] - 1.) / np.log2(i + 2) for i in range(k))
+        sorted_indices = np.argsort(y_score)[::-1]
+        dcg = sum((2 ** y_true[sorted_indices[i]] - 1.) / np.log2(i + 2) for i in range(k))
 
         # return ndcg
         return 1 if ideal_dcg == 0 else float(dcg) / float(ideal_dcg)
